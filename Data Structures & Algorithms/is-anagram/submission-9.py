0bdfe6262fc33aff6617abae6c8a8c3a89407852
Raja Hussain <rjfresh988@gmail.com>
2026-04-28 17:34:50 -0400

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        s_sorted = sorted(s)
+        t_sorted = sorted(t)
+        l = 0 
+        while l < len(s_sorted):
+            if s_sorted[l] != t_sorted[l]:
+                return False
+            l += 1
+        return True
